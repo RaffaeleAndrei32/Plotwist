80a543b47a942cd0f5cacfc09cb485806819c692
Raffaele <raffaeleandrei2002@gmail.com>
2026-01-12 22:57:23 +0100

    added watchlist

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -9,5 +9,9 @@ app_name = 'movies'
 
 urlpatterns = [
     path("", MovieListView.as_view(), name="list_movies"),
+    path("<int:pk>/", MovieDetailView.as_view(), name="movie_info"),
     path("add_movie/", MovieCreateView.as_view(), name="add_movie"),
+    path("watchlist/", WatchlistView.as_view(), name="watchlist"),
+    path("<int:pk>/add_to_watchlist/", AddToWatchlistView.as_view(), name="add_to_watchlist"),
+    path("<int:pk>/remove_from_watchlist/", RemoveFromWatchlistView.as_view(), name="remove_from_watchlist"),
 ]
--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,17 +1,70 @@
-from django.views.generic import CreateView, ListView
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import CreateView, ListView, DetailView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.db.models import Q
+from django.shortcuts import redirect, get_object_or_404
+from django.views import View
+import datetime
 
-from .models import Movie
+from .models import Movie, Director, Genre
 from .forms import MovieForm
 
 
 
 class MovieListView(ListView):
     model = Movie
-    template_name = 'movies/movie_list.html'
+    template_name = 'movies/movies_list.html'
     context_object_name = 'movies'
     paginate_by = 9
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['directors'] = Director.objects.all()
+        context['genres'] = Genre.objects.all()
+        
+        current_year = datetime.date.today().year
+        context['years'] = range(1870, current_year + 10, 10)
+        
+        return context
+    
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        
+        # Filtro per ricerca titolo
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(title__icontains=search)
+        
+        genre_id = self.request.GET.get('genre')
+        if genre_id:
+            queryset = queryset.filter(genres__id=genre_id)
+        
+        director_id = self.request.GET.get('director')
+        if director_id:
+            queryset = queryset.filter(director__id=director_id)
+        
+        year = self.request.GET.get('year')
+        if year:
+            year_int = int(year)
+            decade_end = year_int + 9
+            queryset = queryset.filter(release_date__year__gte=year_int, release_date__year__lte=decade_end)
+        
+        return queryset.distinct()
+
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = 'movies/movie_info.html'
+    context_object_name = 'movie'
+    pk_url_kwarg = 'pk'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Controlla se l'utente ha aggiunto questo film alla watchlist
+        if self.request.user.is_authenticated:
+            context['in_watchlist'] = self.object.watched_by.filter(id=self.request.user.id).exists()
+        return context
 
 
 
@@ -21,11 +74,41 @@ class MovieCreateView(UserPassesTestMixin, CreateView):
     template_name = 'movies/add_movie.html'
     success_url = reverse_lazy('movies:list_movies')
 
-    # Controllo permessi: solo Staff o Moderatori
     def test_func(self):
         return self.request.user.is_staff or self.request.user.groups.filter(name='Moderators').exists()
 
-    # Salvataggio automatico dell'utente loggato
     def form_valid(self, form):
         form.instance.logged_by = self.request.user
         return super().form_valid(form)
+
+
+
+class WatchlistView(LoginRequiredMixin, ListView):
+    model = Movie
+    template_name = 'movies/watchlist.html'
+    context_object_name = 'movies'
+    paginate_by = 9
+    login_url = 'login'
+    
+    def get_queryset(self):
+        return self.request.user.watched_movies.all()
+
+
+
+class AddToWatchlistView(LoginRequiredMixin, View):
+    login_url = 'login'
+    
+    def post(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.watched_by.add(request.user)
+        return redirect('movies:watchlist')
+
+
+
+class RemoveFromWatchlistView(LoginRequiredMixin, View):
+    login_url = 'login'
+    
+    def post(self, request, pk):
+        movie = get_object_or_404(Movie, pk=pk)
+        movie.watched_by.remove(request.user)
+        return redirect('movies:watchlist')
